26158d8c57dfc36434469ef3fa425cce8345e202
Gabriel Young <gabeplaysdrums@live.com>
2014-06-08 12:21:29 -0700

    some optimizations
    Squashed commit of the following:
    
    commit 078721ee56bb790d1726488f1a69fd5fea67edd3
    Author: Gabriel Young <gabeplaysdrums@live.com>
    Date:   Sun Jun 8 12:18:47 2014 -0700
    
        change the default reset interval to 1 minute
    
    commit 111fbf9561d7b5708b3b8311cac1beed68bc93c0
    Author: Gabriel Young <gabeplaysdrums@live.com>
    Date:   Sun Jun 8 12:15:37 2014 -0700
    
        catch exceptions while setting the connection
    
    commit e817e5a3d8d1b1822dda86e1c3bc49c2663bdbaf
    Author: Gabriel Young <gabeplaysdrums@live.com>
    Date:   Sun Jun 8 12:07:15 2014 -0700
    
        print the date and time before each attempt
    
    commit bd075948c702a4b4bef5ced3d326fc522a817bee
    Author: Gabriel Young <gabeplaysdrums@live.com>
    Date:   Sun Jun 8 11:59:58 2014 -0700
    
        only set connection if not connected

--- fix-internet.py
+++ fix-internet.py
@@ -1,9 +1,9 @@
-import requests
+from datetime import datetime
 from optparse import OptionParser
+import requests
 import time
 
 RETRY_SECONDS = 5
-RESET_MINUTES = 15
 
 def parse_command_line():
 
@@ -18,7 +18,7 @@ def parse_command_line():
     )
     
     parser.add_option(
-        '-t', '--interval', dest='reset_minutes', default=15,
+        '-t', '--interval', dest='reset_minutes', default=1,
         help='time between attempts in minutes',
     )
     
@@ -33,66 +33,74 @@ reset_minutes = int(options.reset_minutes)
 
 while (True):
 
-    print 'logging in ...'
-    r = requests.post(
-        'http://192.168.0.1/goform/act_set_web_login',
-        data = {
-            "page": "index.html",
-            "admin_user_name": args[0],
-            "admin_password": args[1],
-        },
-    )
-    
-    if not r.ok:
-        raise Exception('failed to log in')
-    
-    connected = False
-    tries = 5
-    
-    while not connected and tries > 0:
-    
-        print 'setting connection status ...'
+    print '-- ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' --'
+
+    try:
+        print 'logging in ...'
         r = requests.post(
-            'http://192.168.0.1/goform/act_ifx_set_system_status',
+            'http://192.168.0.1/goform/act_set_web_login',
             data = {
-                "page": "modemstatus_real.html",
-                "var:conname": "",
-                "var:contype": "",
-                "connectflag": "2",
+                "page": "index.html",
+                "admin_user_name": args[0],
+                "admin_password": args[1],
             },
         )
         
-        if not (r.ok and 'modemstatus_real.html' in r.text):
-            raise Exception('failed to set connection status')
-        
-        print 'getting connection status ...'
-        r = requests.get('http://192.168.0.1/modemstatus_real.html')
-        
         if not r.ok:
-            raise Exception('failed to get connection status')
-        
-        connected = (
-            'var iphy_conn_state = "1";' in r.text and 
-            'var iisp_conn_state = "1";' in r.text
-        )
+            raise Exception('failed to log in')
     
-        tries -= 1
+        def check_connection():
+            print 'getting connection status ...'
+            r = requests.get('http://192.168.0.1/modemstatus_real.html')
+            
+            if not r.ok:
+                raise Exception('failed to get connection status')
+            
+            return (
+                'var iphy_conn_state = "1";' in r.text and 
+                'var iisp_conn_state = "1";' in r.text
+            )
         
-        print 'connected: %s' % (connected,)
+        connected = check_connection()
+        tries = 5
+        
+        while not connected and tries > 0:
+        
+            print 'setting connection status ...'
+            r = requests.post(
+                'http://192.168.0.1/goform/act_ifx_set_system_status',
+                data = {
+                    "page": "modemstatus_real.html",
+                    "var:conname": "",
+                    "var:contype": "",
+                    "connectflag": "2",
+                },
+            )
+            
+            if not (r.ok and 'modemstatus_real.html' in r.text):
+                raise Exception('failed to set connection status')
     
+            connected = check_connection()
+            tries -= 1
+            
+            print 'connected: %s' % (connected,)
+        
+            if connected:
+                break
+        
+            if tries == 0:
+                break
+        
+            print 'Retrying in %d seconds (%d remaining) ...' % (RETRY_SECONDS, tries)
+            time.sleep(RETRY_SECONDS)
+        
         if connected:
-            break
-    
-        if tries == 0:
-            break
-    
-        print 'Retrying in %d seconds (%d remaining) ...' % (RETRY_SECONDS, tries)
-        time.sleep(RETRY_SECONDS)
-    
-    if connected:
-        print 'Success!'
-    else:
-        print 'Failed!'
+            print 'Success!'
+        else:
+            print 'Failed!'
+
+    except Exception as ex:
+        print 'Warning: encountered an exception: ' + ex
 
     if not options.daemon:
         break
