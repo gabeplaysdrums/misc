5348da1e13110f56fe7ff0760c2fd141ab6add92
Gabriel Young <gabeplaysdrums@live.com>
2014-07-30 20:56:11 -0700

    add path to pyflickr scripts

--- pyflickr/upload.sh
+++ pyflickr/upload.sh
@@ -1,3 +1,5 @@
-#python pyflickr-upload.py --tag=arijascamera "/Users/Gabe/Pictures/Arijas Camera/" -o /Users/Gabe/Skydrive/uploaded.csv --no-movies $*
-#python pyflickr-upload.py --tag=arijascamera "/Users/Gabe/Pictures/Arijas Camera/" -o /Users/Gabe/Skydrive/uploaded.csv --no-pictures --threadpool-size=3 $*
-python pyflickr-upload.py --tag=arijascamera "/Volumes/Untitled/Users/Arija/Desktop/camera contents - sort this" -o /Users/Gabe/Skydrive/uploaded.csv --no-pictures --threadpool-size=1 $*
+PYFLICKR_ROOT=/Users/Gabe/Repos/pyflickr
+
+#python $PYFLICKR_ROOT/pyflickr-upload.py --tag=arijascamera "/Users/Gabe/Pictures/Arijas Camera/" -o /Users/Gabe/Skydrive/uploaded.csv --no-movies $*
+#python $PYFLICKR_ROOT/pyflickr-upload.py --tag=arijascamera "/Users/Gabe/Pictures/Arijas Camera/" -o /Users/Gabe/Skydrive/uploaded.csv --no-pictures --threadpool-size=3 $*
+python $PYFLICKR_ROOT/pyflickr-upload.py --tag=arijascamera "/Volumes/Untitled/Users/Arija/Desktop/camera contents - sort this" -o /Users/Gabe/Skydrive/uploaded.csv --no-pictures --threadpool-size=1 $*
