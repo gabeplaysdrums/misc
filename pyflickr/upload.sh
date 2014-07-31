ba58c2f7dfd31772e7c64f0ab601f03a44e112d2
Gabriel Young <gabeplaysdrums@live.com>
2014-07-30 23:54:48 -0700

    enable upload of new photos

--- pyflickr/upload.sh
+++ pyflickr/upload.sh
@@ -1,5 +1,5 @@
 PYFLICKR_ROOT=/Users/Gabe/Repos/pyflickr
 
-#python $PYFLICKR_ROOT/pyflickr-upload.py --tag=arijascamera "/Users/Gabe/Pictures/Arijas Camera/" -o /Users/Gabe/Skydrive/uploaded.csv --no-movies $*
-#python $PYFLICKR_ROOT/pyflickr-upload.py --tag=arijascamera "/Users/Gabe/Pictures/Arijas Camera/" -o /Users/Gabe/Skydrive/uploaded.csv --no-pictures --threadpool-size=3 $*
-python $PYFLICKR_ROOT/pyflickr-upload.py --tag=arijascamera "/Volumes/Untitled/Users/Arija/Desktop/camera contents - sort this" -o /Users/Gabe/Skydrive/uploaded.csv --no-pictures --threadpool-size=1 $*
+python $PYFLICKR_ROOT/pyflickr-upload.py --tag=arijascamera "/Users/Gabe/Pictures/Arijas Camera/" -o /Users/Gabe/Skydrive/uploaded.csv --no-movies $*
+python $PYFLICKR_ROOT/pyflickr-upload.py --tag=arijascamera "/Users/Gabe/Pictures/Arijas Camera/" -o /Users/Gabe/Skydrive/uploaded.csv --no-pictures --threadpool-size=3 $*
+#python $PYFLICKR_ROOT/pyflickr-upload.py --tag=arijascamera "/Volumes/Untitled/Users/Arija/Desktop/camera contents - sort this" -o /Users/Gabe/Skydrive/uploaded.csv --no-pictures --threadpool-size=1 $*
