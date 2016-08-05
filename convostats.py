4b93a60fa713c862fc855095bd3320ae7470ac46
Gabriel Young <gabeplaysdrums@live.com>
2016-08-04 20:40:29 -0700

    parse purple txt logs

--- convostats.py
+++ convostats.py
@@ -62,14 +62,8 @@ def parse_command_line():
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-#TODO: remove
-#current_tag = None
-
 
 def process_html(path):
-    #TODO: remove
-    #global current_tag
-
     def gaim_extract_date(soup):
         if soup.title:
             m = re.match(r'Conversation with .* at (\d{4})-(\d{2})-(\d{2}) \d{2}:\d{2}:\d{2} on .* \(aim\)', soup.title.get_text())
@@ -199,6 +193,68 @@ def process_html(path):
             print('unrecognized log format')
 
 
+def process_txt(path):
+    def purple_extract_date(line):
+        if line:
+            m = re.match(r'Conversation with .* at (\d\d?)/(\d\d?)/(\d{4}) \d\d?:\d{2}:\d{2} (AM|PM) on .* \(aim\)', line)
+            if m:
+                year = int(m.group(3))
+                month = int(m.group(1))
+                day = int(m.group(2))
+                return date(year, month, day)
+        return None
+
+    def purple_extract(line):
+        curr_time = None
+        curr_alias = None
+        curr_message_part = None
+        if line and '<AUTO-REPLY>' not in line:
+            m = re.match(r'^\((\d\d?):(\d{2}):(\d{2})\s+(AM|PM)\) ([a-zA-Z0-9 ]+): (.*)$', line)
+            if m:
+                hours = int(m.group(1))
+                minutes = int(m.group(2))
+                seconds = int(m.group(3))
+                if m.group(4) == 'PM':
+                    hours += 12
+
+                #TODO: bug! message occurred the next day
+                if hours > 23:
+                    hours -= 24
+
+                curr_time = time(hours, minutes, seconds)
+                curr_alias = m.group(5).strip()
+                curr_message_part = m.group(6)
+            else:
+                curr_message_part = line
+        return curr_time, curr_alias, curr_message_part
+
+    with open(path) as fin:
+        first_line = fin.readline()
+        purple_date = purple_extract_date(first_line)
+        if purple_date:
+            print('purple log detected')
+
+            timestamp = None
+            alias = None
+            message = None
+
+            for line in fin:
+                curr_time, curr_alias, curr_message_part = purple_extract(line)
+                if curr_time and curr_alias:
+                    if timestamp and alias and message:
+                        yield alias, message.strip(), timestamp
+                    timestamp = datetime.combine(purple_date, curr_time)
+                    alias = curr_alias
+                    message = curr_message_part
+                elif curr_message_part:
+                    message += curr_message_part
+
+            if timestamp and alias and message:
+                yield alias, message.strip(), timestamp
+        else:
+            print('unrecognized log format')
+
+
 SUBSTITUTES = (
     ('gues', 'guess'),
     ('ye', 'yes'),
@@ -226,6 +282,7 @@ SUBSTITUTES = (
     ("it'll", 'it will'),
     ('dont', "don't"),
     ("dont'", "don't"),
+    ("don't'", "don't"),
     ('cuz', 'because'),
     ('np', 'no problem'),
     ('lemme', 'let me'),
@@ -235,44 +292,60 @@ SUBSTITUTES = (
     ('convo', 'conversation'),
     ('ko', 'ok'),
     ('kk', 'ok'),
+    ('sok', "it's ok"),
     (r'aw[w]*', 'aww'),
     (r'yo[o]*', 'yo'),
     ('ap', 'app'),
     ('discovercard', 'discover card'),
     ('ttyl', 'talk to you later'),
     ('watcha', 'what are you'),
+    (r'oo[o]*[h]?', 'ooh'),
 )
 
 
 def main(options, args):
     input_path = args[0]
-    message_count = 0
-    message_text = ''
+
+    class Context:
+        def __init__(self):
+            self.message_count = 0
+            self.message_text = ''
+    ctx = Context()
+    del Context
 
     if os.path.isdir(input_path):
-        for (root, dirs, files) in os.walk(os.path.abspath(input_path)):
-            html_files = list(fnmatch.filter(files, '*.html')) + list(fnmatch.filter(files, '*.htm'))
-            for name in html_files:
+        def process_files(root, names, fn):
+            for name in names:
                 path = os.path.join(root, name)
                 print('Processing %s ...' % (path,))
-                for alias, message, timestamp in process_html(path):
-                    message_count += 1
-                    message_text += message + '\n'
-                    #print('alias=', alias, 'message=', message, 'timestamp=', timestamp)
+                for alias, message, timestamp in fn(path):
+                    #print('alias=', alias, 'message=', repr(message), 'timestamp=', timestamp)
+                    try:
+                        message = message.encode('ascii', 'ignore')
+                        ctx.message_count += 1
+                        ctx.message_text += message + '\n'
+                    except UnicodeDecodeError:
+                        pass
 
-        print('Processed %d messages' % message_count)
+        for (root, dirs, files) in os.walk(os.path.abspath(input_path)):
+            html_files = list(fnmatch.filter(files, '*.html')) + list(fnmatch.filter(files, '*.htm'))
+            process_files(root, html_files, process_html)
+            txt_files = fnmatch.filter(files, '*.txt')
+            process_files(root, txt_files, process_txt)
+
+        print('Processed %d messages' % ctx.message_count)
 
         if options.message_text_output_path:
             with open(os.path.normpath(options.message_text_output_path), 'w') as f:
-                f.write(message_text.encode('utf8'))
+                f.write(ctx.message_text)
     else:
         with open(input_path, 'r') as f:
-            message_text = f.read().decode('utf8')
+            ctx.message_text = f.read()
 
-    message_text = re.sub('\d*', '', message_text)
+    ctx.message_text = re.sub('\d*', '', ctx.message_text)
 
     for a, b in SUBSTITUTES:
-        message_text = re.sub(r'\b' + a + r'\b', b, message_text, flags=re.IGNORECASE | re.MULTILINE)
+        ctx.message_text = re.sub(r'\b' + a + r'\b', b, ctx.message_text, flags=re.IGNORECASE | re.MULTILINE)
 
     stopwords = {
         'yeah',
@@ -326,7 +399,7 @@ def main(options, args):
 
     stopwords.update(STOPWORDS)
 
-    wordcloud = WordCloud(width=1280, height=720, max_words=1000, stopwords=stopwords).generate(message_text)
+    wordcloud = WordCloud(width=1280, height=720, max_words=1000, stopwords=stopwords).generate(ctx.message_text)
 
     for word, weight in sorted(wordcloud.words_, key=lambda x: x[1], reverse=True):
         print('%-30s %.6f' % (word, weight))
